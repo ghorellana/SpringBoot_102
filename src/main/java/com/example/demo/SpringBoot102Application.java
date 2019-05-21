840e1fe60a2b023f2658196889e8c414f21c96e5
ghorellana <ghorellana@gmail.com>
2019-05-21 10:16:35 -0400

    This is update

--- src/main/java/com/example/demo/SpringBoot102Application.java
+++ src/main/java/com/example/demo/SpringBoot102Application.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 @SpringBootApplication
 public class SpringBoot102Application {
 
@@ -11,3 +14,4 @@ public class SpringBoot102Application {
 	}
 
 }
+
